dd34dd0ed1ac2e77bab8c05ba6b01cb78f1b8cd6
Edin Omeragic <edin.omeragic@gmail.com>
2020-05-10 11:46:12 +0200

    Removed ActionFilter

--- app/Filters/ActionFilter.php
+++ app/Filters/ActionFilter.php
@@ -5,14 +5,15 @@ namespace App\Filters;
 use Atom\Router\Action;
 use Atom\Router\IActionFilter;
 
-class ActionFilter implements IActionFilter
-{
-    public function filter(Action $action)
-    {
-        return [
-            "message" => "Hello from action filter",
-            "controller" => $action->getRoute()->getActionHandler()->getController(),
-            "method" => $action->getRoute()->getActionHandler()->getMethodName()
-        ];
-    }
-}
+// TODO: Rewrite using Middleware
+// class ActionFilter implements IActionFilter
+// {
+//     public function filter(Action $action)
+//     {
+//         return [
+//             "message" => "Hello from action filter",
+//             "controller" => $action->getRoute()->getActionHandler()->getController(),
+//             "method" => $action->getRoute()->getActionHandler()->getMethodName()
+//         ];
+//     }
+// }
