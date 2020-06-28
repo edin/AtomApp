22b96dd36568db980d960596f698304b0073b5b9
Edin Omeragic <edin.omeragic@gmail.com>
2020-06-28 22:01:29 +0200

    Testing

--- app/Controllers/Admin/CategoryController.php
+++ app/Controllers/Admin/CategoryController.php
@@ -9,83 +9,6 @@ use App\ViewModels\TableViewModel;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-final class SortOrder
-{
-    public string $fieldName = "";
-    public string $direction = "asc";
-}
-
-final class Field
-{
-    public string $fieldName;
-    public string $title;
-    public bool   $sortable = false;
-}
-
-final class FieldCollection
-{
-    private array $fields = [];
-
-    public function add(Field $field): void
-    {
-        $this->fields[] = $field;
-    }
-
-    // public function 
-
-}
-
-final class SortCollection
-{
-    private array $order = [];
-
-    public function add(SortOrder $order)
-    {
-        $this->order[$order->fieldName] = $order;
-    }
-
-    public function remove(SortOrder $order)
-    {
-        unset($this->order[$order->fieldName]);
-    }
-
-    public function removeBy(string $fieldName)
-    {
-        unset($this->order[$fieldName]);
-    }
-
-    public function fromString(string $order)
-    {
-        $parts = explode(",", $order);
-        foreach ($parts as $part) {
-            $subParts = explode(".", $part);
-            $fieldName = $subParts[0] ?? "";
-            $fieldOrder = $subParts[1] ?? "asc";
-            if ($fieldOrder !== "desc") {
-                $fieldOrder = "asc";
-            }
-            $orderBy = new SortOrder();
-            $orderBy->fieldName = $fieldName;
-            $orderBy->direction = $fieldOrder;
-            $this->add($orderBy);
-        }
-    }
-
-    public function items(): array
-    {
-        return $this->order;
-    }
-
-    public function getSortDirection(string $fieldName): string
-    {
-        if (isset($this->order[$fieldName])) {
-            return $this->order[$fieldName]->direction;
-        }
-        return "";
-    }
-}
-
-
 final class CategoryController
 {
     private CategoryRepository $repository;
@@ -105,7 +28,7 @@ final class CategoryController
         //$this->viewModel->setViewPath("")
     }
 
-    public function index(int $page = 1, ?string $orderBy = null)
+    public function index(int $page = 1, ?string $orderBy = null, ?string $filterBy = null)
     {
         $this->viewModel->setPage($page);
         $this->viewModel->setOrder($orderBy);
--- app/ViewModels/TableViewModel.php
+++ app/ViewModels/TableViewModel.php
@@ -2,6 +2,80 @@
 
 namespace App\ViewModels;
 
+final class SortOrder
+{
+    public string $fieldName = "";
+    public string $direction = "asc";
+}
+
+final class Field
+{
+    public string $fieldName;
+    public string $title;
+    public bool   $sortable = false;
+    public bool   $filterable = false;
+}
+
+final class FieldCollection
+{
+    private array $fields = [];
+
+    public function add(Field $field): void
+    {
+        $this->fields[] = $field;
+    }
+}
+
+final class SortCollection
+{
+    private array $order = [];
+
+    public function add(SortOrder $order)
+    {
+        $this->order[$order->fieldName] = $order;
+    }
+
+    public function remove(SortOrder $order)
+    {
+        unset($this->order[$order->fieldName]);
+    }
+
+    public function removeBy(string $fieldName)
+    {
+        unset($this->order[$fieldName]);
+    }
+
+    public function fromString(string $order)
+    {
+        $parts = explode(",", $order);
+        foreach ($parts as $part) {
+            $subParts = explode(".", $part);
+            $fieldName = $subParts[0] ?? "";
+            $fieldOrder = $subParts[1] ?? "asc";
+            if ($fieldOrder !== "desc") {
+                $fieldOrder = "asc";
+            }
+            $orderBy = new SortOrder();
+            $orderBy->fieldName = $fieldName;
+            $orderBy->direction = $fieldOrder;
+            $this->add($orderBy);
+        }
+    }
+
+    public function items(): array
+    {
+        return $this->order;
+    }
+
+    public function getSortDirection(string $fieldName): string
+    {
+        if (isset($this->order[$fieldName])) {
+            return $this->order[$fieldName]->direction;
+        }
+        return "";
+    }
+}
+
 final class TableViewModel
 {
     private int $page = 1;
