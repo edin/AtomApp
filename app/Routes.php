09f6f96f64371d5f525f10b5c85dd6ab57e6111a
Edin Omeragic <edin.omeragic@gmail.com>
2021-05-01 10:29:38 +0200

    Updated route method names

--- app/Routes.php
+++ app/Routes.php
@@ -10,7 +10,6 @@ use App\Middlewares\LogMiddleware;
 use App\Controllers\HomeController;
 use App\Controllers\AccountController;
 use App\Controllers\Admin\CategoryController;
-use App\Controllers\Admin\PostController;
 use App\Controllers\ValidationController;
 use App\Controllers\Link\LinkController;
 use App\Middlewares\AuthMiddleware;
@@ -19,9 +18,9 @@ class Routes
 {
     public function configure(Router $router)
     {
-        $router->addGroup("/", function (Router $group) {
-            $group->addMiddleware(LogMiddleware::class);
-            $group->addMiddleware(AuthMiddleware::class);
+        $router->group("/", function (Router $group) {
+            $group->middleware(LogMiddleware::class);
+            $group->middleware(AuthMiddleware::class);
             $group->setController(HomeController::class);
 
             $group->get("", "index");
@@ -32,10 +31,10 @@ class Routes
             $group->attach(RouteBuilder::fromController(AccountController::class));
         });
 
-        $router->addGroup("/admin", function (Router $group) {
-            $group->addMiddleware(LogMiddleware::class);
+        $router->group("/admin", function (Router $group) {
+            $group->middleware(LogMiddleware::class);
 
-            $group->addGroup("/admin/category", function (Router $group) {
+            $group->group("/admin/category", function (Router $group) {
                 $group->setController(CategoryController::class);
                 $group->get("",  "index");
                 $group->getOrPost("create");
@@ -47,13 +46,19 @@ class Routes
         $this->attach($router, "/api", ApiController::class);
         $this->attach($router, "/", LinkController::class);
 
-        $router->get("/api/users-all", function (UserRepository $users) {
-            return $users->findAll();
-        });
+        // $router->get("/api/users", function (UserRepository $users) {
+        //     return $users->findAll();    
+        // });
+
+        $router->get("/api/users", [Routes::class, "getUsers"]);
     }
 
-    private function attach($router, $path, $controller)
+    private function attach($router, $path, $controller) 
     {
         $router->attachTo($path,  RouteBuilder::fromController($controller));
+    }
+
+    public function getUsers(UserRepository $users) {
+        return $users->findAll();
     }
 }
